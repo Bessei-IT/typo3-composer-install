7d096cd7b7af9d3ff73b8641b694507411eb824c
Christoph Bessei <chris@bessei-it.eu>
2017-05-25 16:36:14 +0200

    Remove unused import

--- src/BIT/Command/InstallCommand.php
+++ src/BIT/Command/InstallCommand.php
@@ -8,7 +8,6 @@ use Symfony\Component\Console\Formatter\OutputFormatter;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Process\ProcessUtils;
 
